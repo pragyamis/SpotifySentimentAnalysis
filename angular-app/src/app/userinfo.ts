a5eb14f0a4e5e127dbe78f89f7eca229890e5eba
vm9800 <vm9800@att.com>
2019-12-07 21:16:25 -0500

    adding missing files

---
+++ angular-app/src/app/userinfo.ts
@@ -0,0 +1,24 @@
+
+export interface IUserInfo {
+    "username" : string;
+    "history" : IHistory[];
+}
+
+export interface IHistory {
+    "timestamp" : string;
+    "songs" : ISongs[];
+}
+
+export interface ISongs {
+    "name" : string;
+    "sentiment" : string;
+ }
+
+ export class ISongSentimentData {
+    date : Date;
+    pct25 : number;
+    pct50 : number;
+    pct05 : number;
+    pct75 : number;
+    pct95 : number;
+}
